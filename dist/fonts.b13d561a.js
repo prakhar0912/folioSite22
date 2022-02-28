81b53f17a4969895c921a75f775b8e5d941e2c53
prakhar <prakhar0912@gmail.com>
2022-02-28 16:14:48 +0530

    height issue, padding fix, about me change

--- dist/fonts.b13d561a.js
+++ dist/fonts.b13d561a.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43069" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36497" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
