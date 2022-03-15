747f62505036f1f5b04d0945f10dc8fd1deef981
prakhar <prakhar0912@gmail.com>
2022-03-15 20:03:28 +0530

    fixing scroll bar issues

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -49692,7 +49692,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36497" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34005" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
